c34184fb5b100ee45cde6ed50e8368a4549bf633
ZAIN ZAHID <zaynzahid2000@gmail.com>
2021-06-07 23:03:38 +0530

    deployment changes

--- ecom/settings.py
+++ ecom/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#6t349uradd1q((()d^4_spi2#ms8ts+n40dw*7r6sopxcv($m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "ecom/static")
 
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- ecom/urls.py
+++ ecom/urls.py
@@ -19,11 +19,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+from django.conf.urls import url
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='Home'),
     path('blog/', include("blog.urls")),
     path('shop/', include("shop.urls")),
   
-
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
